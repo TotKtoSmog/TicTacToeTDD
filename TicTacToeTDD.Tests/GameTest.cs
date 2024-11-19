059ba761507ff24c62575b986156ab69d801f2ca
Alexey Ustinov <TotKtoSmog513@gmail.com>
2024-11-19 04:28:11 +0300

    Второй этап. Создание автоматического теста для получения значения клетки игрового поля и кода для прохождения данного теста

--- TicTacToeTDD.Tests/GameTest.cs
+++ TicTacToeTDD.Tests/GameTest.cs
@@ -11,6 +11,32 @@
             string actual = game.CreateMap();
             Assert.AreEqual(expected, actual);
         }
+        public static IEnumerable<object[]> GetValuesToPosition =>
+        [
+            [0, 0, "■"],
+            [0, 1, "■"],
+            [0, 2, "■"],
+            [1, 0, "■"],
+            [1, 1, "■"],
+            [1, 2, "■"],
+            [2, 0, "■"],
+            [2, 1, "■"],
+            [2, 2, "■"],
+        ];
+
+        [DataTestMethod]
+        [DynamicData(nameof(GetValuesToPosition))]
+        public void GetValuesToPosition_Correctly(int x, int y, string p)
+        {
+            Game game = new();
+
+            game.CreateMap();
+
+            string actual = game.GetValuesToPosition(x, y);
+
+            Assert.AreEqual(p, actual);
+        }
+
 
     }
 }
--- TicTacToeTDD/Game.cs
+++ TicTacToeTDD/Game.cs
@@ -2,6 +2,21 @@
 {
     public class Game
     {
-        public string CreateMap() => "■■■\n■■■\n■■■\n";
+        public string[,] Board = new string[3, 3];
+        public string CreateMap()
+        {
+            string result = "";
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Board[i, j] = "■";
+                    result += "■";
+                }
+                result += "\n";
+            }
+            return result;
+        }
+        public string GetValuesToPosition(int x, int y) => Board[x, y];
     }
 }
